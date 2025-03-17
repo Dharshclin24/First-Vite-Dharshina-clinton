918172d4e0c01c799d5652708d32831d77b4ea8a
Dharshclin24 <shavonfdclinton@gmail.com>
2025-03-17 13:53:53 -0700

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function About() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>This is about the page
+        
+      </p>
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function Home() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>This is the Home Page</p>
+    </>
+  );
+}
+
+export default Home;
